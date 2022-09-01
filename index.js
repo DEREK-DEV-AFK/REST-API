0ec3d6f03e31dc50f6940e665ed5fcf94d42477d
DEREK-DEV-AFK <sufiyanmemon687@gmail.com>
2022-09-01 22:15:56 +0530

    added joi and completed REST api basics

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express');
+const Joi = require('joi'); // it is an class
+const express = require('express'); // it is an class
 const app = express();
+
 
 
 app.use(express.json());
@@ -36,7 +38,7 @@ app.get('/api/courses', (req, res) => {
 
 // /api/courses/1 here :id can be listen by params
 app.get('/api/courses/:id', (req, res) => {
-    //res.send(req.params.id);
+    //res.send(req.params.id); 
 
     // finding that if that course exist by caamparing
     const course =  courses.find(c => c.id === parseInt(req.params.id));
@@ -54,13 +56,68 @@ app.get('/api/courses/:id/:year', (req, res) => {
 // POST
 
 app.post('/api/courses', (req, res) => {
+    // you should never trust client, you should always validate the input
+    const {error} /*object destructuring equivalent to get result.error*/ = validateCourse(req.body);
+    //console.log(result);
+    
+    
+    if(error){
+        // 400 bad request
+        res.status(400).send(error.details[0].message);
+        return; // to end 
+    }
     const course = {
         id: courses.length+1,
         name: req.body.name
     };
     courses.push(course);
     res.send(course);
-})
+});
+
+app.put('/api/courses/:id',(req,res) => {
+    // loook up the couse
+    // if not existing, return 404 means resource not found
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course)  {
+        res.status(404).send('invlaid course ID');
+        return;
+    }
+    // validate
+    // if invalid, return 400 means bad request
+    const { error } /*object destructuring equivalent to get result.error*/ = validateCourse(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+        
+
+    // update course
+    course.name = req.body.name;
+    // return the updated course
+    res.send(course);
+
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+};
+
+app.delete('/api/courses/:id', (req,res) => {
+    // look up the course
+    // not exsting, retun 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('invlaid course ID');
+        
+
+    // delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // return the same course
+    res.send(course);
+});
 
 
 // setting up the app to listen on
